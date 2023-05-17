caf8741e1fbe108acf67fdf43da0c1ebfea229e8
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-17 02:14:30 -0400

    Initial Commit

---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Lori Rothermel on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
